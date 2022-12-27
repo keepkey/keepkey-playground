169153218d7bdb4710ff5e7f78a4bbda9afe36a9
highlander <bithighlander@gmail.com>
2022-12-27 13:50:53 -0600

    begin dash dapp

--- dapps/bitcoin-dapp/src/App.js
+++ dapps/bitcoin-dapp/src/App.js
@@ -1,22 +1,96 @@
-import logo from './logo.svg';
+import logo from './dash.png';
 import './App.css';
+import { KeepKeySdk } from '@keepkey/keepkey-sdk'
+import {
+    useState,
+    useEffect,
+} from 'react';
+
+let pioneerApi = require("@pioneer-platform/pioneer-client")
+const coinSelect = require('coinselect')
+
 
 function App() {
+    const [address, setAddress] = useState('')
+    const [balance, setBalance] = useState('0.000')
+
+    let onStart = async function(){
+        try{
+            const configPioneer = {
+                queryKey:'sdk:2d0ec79c-6733-4235-9b09-9b87171edc16',
+                username:"bitcoin-example-keepkey",
+                //spec:"https://pioneers.dev/spec/swagger.json"
+                spec:"http://localhost:9001/spec/swagger.json"
+            }
+            let pioneer = new pioneerApi(configPioneer.spec,configPioneer)
+            pioneer = await pioneer.init()
+            console.log("checkpoint1")
+
+            // let globals = await pioneer.Globals()
+            // console.log("globals",globals.data)
+
+            let spec = 'http://localhost:1646/spec/swagger.json'
+            let apiKey = localStorage.getItem("apiKey");
+            let config = {
+                apiKey: apiKey || 'test-123',
+                pairingInfo:{
+                    name: process.env['SERVICE_NAME'] || 'DASH',
+                    imageUrl: process.env['SERVICE_IMAGE_URL'] || 'https://assets.coincap.io/assets/icons/dash@2x.png',
+                    basePath:spec
+                }
+            }
+
+            //init
+            let sdk = await KeepKeySdk.create(config)
+            console.log("config: ",config.apiKey)
+            console.log("checkpoint2")
+
+            let path =
+                    {
+                        symbol: 'DASH',
+                        address_n: [0x80000000 + 44, 0x80000000 + 5, 0x80000000 + 0],
+                        coin: 'Bitcoin',
+                        script_type: 'p2pkh',
+                        showDisplay: false
+                    }
+
+
+
+            let responsePubkey = await sdk.system.info.getPublicKey(path)
+            console.log("responsePubkey: ", responsePubkey)
+            console.log("responsePubkey: ", responsePubkey.xpub)
+
+            //get balance DASH
+            let data = await pioneer.ListUnspent({network:'DASH',xpub:responsePubkey.xpub})
+            data = data.data
+            console.log("txData: ",data)
+
+            let balance = 0
+            for(let i = 0; i < data.length; i++){
+                balance += parseInt(data[i].value)
+            }
+            console.log("balance: ",balance)
+            let balanceNative = balance / 100000000
+            setBalance(balanceNative)
+        }catch(e){
+            console.log(e)
+            //console.error(e)
+        }
+    }
+
+    // onStart()
+    useEffect(() => {
+        onStart()
+    }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Balance: {balance}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         address:
-        </a>
+
       </header>
     </div>
   );
--- tests/get-pubkeys.js
+++ tests/get-pubkeys.js
@@ -1,81 +1,86 @@
 
-const { getKeepKeySDK } = require("../lib")
+const { KeepKeySdk } = require("@keepkey/keepkey-sdk")
+
 
 let spec = 'http://localhost:1646/spec/swagger.json'
 
 let run_test = async function () {
     try {
         let config = {
-            serviceKey: process.env['SERVICE_KEY'] || 'abc-123',
-            serviceName: process.env['SERVICE_NAME'] || 'KeepKey SDK Demo App',
-            serviceImageUrl: process.env['SERVICE_IMAGE_URL'] || 'https://github.com/BitHighlander/keepkey-desktop/raw/master/electron/icon.png',
-            spec
+            apiKey: process.env['SERVICE_KEY'] || 'test-123',
+            pairingInfo:{
+                name: process.env['SERVICE_NAME'] || 'KeepKey SDK Demo App',
+                imageUrl: process.env['SERVICE_IMAGE_URL'] || 'https://github.com/BitHighlander/keepkey-desktop/raw/master/electron/icon.png',
+                basePath:spec
+            }
         }
+
         //init
-        const sdk = await getKeepKeySDK(config)
+        let sdk = await KeepKeySdk.create(config)
+        console.log(config)
 
 
         //Unsigned TX
         let paths = [
             {
                 symbol: 'BTC',
-                addressNList: [ 2147483732, 2147483648, 2147483648 ],
+                address_n: [ 2147483732, 2147483648, 2147483648 ],
                 coin: 'Bitcoin',
                 script_type: 'p2pkh',
                 showDisplay: false
             },
             {
                 symbol: 'ETH',
-                addressNList: [ 2147483692, 2147483708, 2147483648 ],
+                address_n: [ 2147483692, 2147483708, 2147483648 ],
                 coin: 'Bitcoin',
                 script_type: 'p2pkh',
                 showDisplay: false
             },
             {
                 symbol: 'RUNE',
-                addressNList: [ 2147483692, 2147484579, 2147483648 ],
+                address_n: [ 2147483692, 2147484579, 2147483648 ],
                 coin: 'Bitcoin',
                 script_type: 'p2pkh',
                 showDisplay: false
             },
             {
                 symbol: 'ATOM',
-                addressNList: [ 2147483692, 2147483766, 2147483648 ],
+                address_n: [ 2147483692, 2147483766, 2147483648 ],
                 coin: 'Bitcoin',
                 script_type: 'p2pkh',
                 showDisplay: false
             },
             {
                 symbol: 'OSMO',
-                addressNList: [ 2147483692, 2147483766, 2147483648 ],
+                address_n: [ 2147483692, 2147483766, 2147483648 ],
                 coin: 'Bitcoin',
                 script_type: 'p2pkh',
                 showDisplay: false
             },
             {
                 symbol: 'BNB',
-                addressNList: [ 2147483692, 2147484362, 2147483648 ],
+                address_n: [ 2147483692, 2147484362, 2147483648 ],
                 coin: 'Bitcoin',
                 script_type: 'p2pkh',
                 showDisplay: false
             },
             {
                 symbol: 'BCH',
-                addressNList: [ 2147483692, 2147483793, 2147483648 ],
+                address_n: [ 2147483692, 2147483793, 2147483648 ],
                 coin: 'Bitcoin',
                 script_type: 'p2pkh',
                 showDisplay: false
             },
             {
                 symbol: 'LTC',
-                addressNList: [ 2147483692, 2147483650, 2147483648 ],
+                address_n: [ 2147483692, 2147483650, 2147483648 ],
                 coin: 'Bitcoin',
                 script_type: 'p2pkh',
                 showDisplay: false
             },
             {
                 symbol: 'DOGE',
-                addressNList: [ 2147483692, 2147483651, 2147483648 ],
+                address_n: [ 2147483692, 2147483651, 2147483648 ],
                 coin: 'Bitcoin',
                 script_type: 'p2pkh',
                 showDisplay: false
@@ -85,8 +90,8 @@ let run_test = async function () {
         //push tx to api
         // console.log(kk.instance.SignTransaction())
         let timeStart = new Date().getTime()
-        let responseSign = await sdk.wallet.getPublicKeys({ getPublicKey: paths })
-        console.log("responseSign: ", responseSign.data)
+        let responseSign = await sdk.system.info.getPublicKey(paths[0])
+        console.log("responseSign: ", responseSign)
         let timeEnd = new Date().getTime()
         console.log("duration: ",(timeStart - timeEnd) / 1000)
         // let responseSign = await kk.instance.GetPublicKeys(null, { paths })
